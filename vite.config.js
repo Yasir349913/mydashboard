312d0aef421ddd69560cc1bb2a2eafb2c2a8dca5
Yasir349913 <yasirmaqsood534@gmail.com>
2025-07-11 05:43:50 -0700

    deploy on vercel fixing

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
-  base: './',  // relative base path for deployment
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'gsap'  // yahan apne problem wala module daal do
-      ]
-    }
-  }
 })
